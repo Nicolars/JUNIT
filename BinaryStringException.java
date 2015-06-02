fe264e79476983a4bbd69a3e57460ef35d253513
Nicolars <nicolasdelamonja@gmail.com>
2015-06-02 19:47:03 +0200

    Pasar binarystringexception

---
+++ BinaryStringException.java
@@ -0,0 +1,11 @@
+package prJUnit;
+
+public class BinaryStringException extends RuntimeException{
+	public BinaryStringException(){
+		super();
+	}
+	
+	public BinaryStringException(String msg){
+		super(msg);
+	}
+}
